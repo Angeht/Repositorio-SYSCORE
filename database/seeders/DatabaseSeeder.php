a19f8979b986f0488a9d91519a752124cfb00c88
Jeanpier Johann Maza Tamariz <jeanspierm.19jt@gmail.com>
2026-07-02 00:09:33 -0500

    add: use User in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\librerias;
+use App\Models\User;
 use App\Models\SiteContent;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
